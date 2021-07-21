5df812b975589c1f5dec338798dfe146ea7ac81a
aaronsebastian07 <73086898+aaronsebastian07@users.noreply.github.com>
2021-07-20 17:59:31 -0700

    Add files via upload

--- height-weight.py
+++ height-weight.py
@@ -23,18 +23,26 @@ weight_std_deviation = statistics.stdev(weight_list)
 height_first_std_deviation_start, height_first_std_deviation_end = height_mean-height_std_deviation, height_mean+height_std_deviation
 height_second_std_deviation_start, height_second_std_deviation_end = height_mean-(2*height_std_deviation), height_mean+(2*height_std_deviation)
 height_third_std_deviation_start, height_third_std_deviation_end = height_mean-(3*height_std_deviation), height_mean+(3*height_std_deviation)
+#n = any number and standard deviation for the nth height
+#height_nth_std_deviation_start, height_nth_std_deviation_end = height_mean-(nth*height_std_deviation), height_mean+(nth*height_std_deviation) 
 #1, 2 and 3 Standard Deviations for weight
 weight_first_std_deviation_start, weight_first_std_deviation_end = weight_mean-weight_std_deviation, weight_mean+weight_std_deviation
 weight_second_std_deviation_start, weight_second_std_deviation_end = weight_mean-(2*weight_std_deviation), weight_mean+(2*weight_std_deviation)
 weight_third_std_deviation_start, weight_third_std_deviation_end = weight_mean-(3*weight_std_deviation), weight_mean+(3*weight_std_deviation)
+#n = any number and standard deviation for the nth weight
+#weight_nth_std_deviation_start, weight_nth_std_deviation_end = weight_mean-(nth*weight_std_deviation), weight_mean+(nth*weight_std_deviation) 
 #Percentage of data within 1, 2 and 3 Standard Deviations for Height
 height_list_of_data_within_1_std_deviation = [result for result in height_list if result > height_first_std_deviation_start and result < height_first_std_deviation_end]
 height_list_of_data_within_2_std_deviation = [result for result in height_list if result > height_second_std_deviation_start and result < height_second_std_deviation_end]
 height_list_of_data_within_3_std_deviation = [result for result in height_list if result > height_third_std_deviation_start and result < height_third_std_deviation_end]
+#% of data is within the nth standard deviation for height
+#height_list_of_data_within_nth_std_deviation = [result for result in height_list if result > height_nth_std_deviation_start and result < height_nth_std_deviation_end]
 #Percentage of data within 1, 2 and 3 Standard Deviations for Weight
 weight_list_of_data_within_1_std_deviation = [result for result in weight_list if result > weight_first_std_deviation_start and result < weight_first_std_deviation_end]
 weight_list_of_data_within_2_std_deviation = [result for result in weight_list if result > weight_second_std_deviation_start and result < weight_second_std_deviation_end]
 weight_list_of_data_within_3_std_deviation = [result for result in weight_list if result > weight_third_std_deviation_start and result < weight_third_std_deviation_end]
+#% of data is within the nth standard deviation for weight
+#weight_list_of_data_within_nth_std_deviation = [result for result in weight_list if result > weight_nth_std_deviation_start and result < weight_nth_std_deviation_end]
 #Printing data for height and weight (Standard Deviation)
 print("{}% of data for height lies within 1 standard deviation".format(len(height_list_of_data_within_1_std_deviation)*100.0/len(height_list)))
 print("{}% of data for height lies within 2 standard deviations".format(len(height_list_of_data_within_2_std_deviation)*100.0/len(height_list)))
@@ -42,3 +50,6 @@ print("{}% of data for height lies within 3 standard deviations".format(len(heig
 print("{}% of data for weight lies within 1 standard deviation".format(len(weight_list_of_data_within_1_std_deviation)*100.0/len(weight_list)))
 print("{}% of data for weight lies within 2 standard deviations".format(len(weight_list_of_data_within_2_std_deviation)*100.0/len(weight_list)))
 print("{}% of data for weight lies within 3 standard deviations".format(len(weight_list_of_data_within_3_std_deviation)*100.0/len(weight_list)))
+#printing the data for height and weight of nth standard deviation
+#print("{}% of data for height lies within nth standard deviation".format(len(height_list_of_data_within_nth_std_deviation)*100.0/len(height_list)))
+#print("{}% of data for weight lies within nth standard deviations".format(len(weight_list_of_data_within_nth_std_deviation)*100/len(weight_list)))
